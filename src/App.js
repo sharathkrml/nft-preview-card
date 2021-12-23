e76dd01d89aedd852dd3c755b5e43640bb5a9793
sharath V <sharathkrml@gmail.com>
2021-12-23 23:31:46 +0530

    added hover effects

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import eqi from "../src/assets/image-equilibrium.jpg";
 import avatar from "../src/assets/image-avatar.png";
 import eth from "../src/assets/icon-ethereum.svg";
 import clock from "../src/assets/icon-clock.svg";
+import view from "../src/assets/icon-view.svg";
 
 import "./App.scss";
 
@@ -12,6 +13,9 @@ function App() {
       <div className="main-div">
         <div className="main-img">
           <img src={eqi} alt="eqilibrium" />
+          <div class="middle">
+            <img src={view} alt="view" />
+          </div>
         </div>
         <div className="main-head">Equilibrium #3429</div>
         <div className="desc">
@@ -20,7 +24,7 @@ function App() {
         <div className="eth-div">
           <div className="price">
             <img src={eth} alt="eth" />
-             0.041 ETH
+            0.041 ETH
           </div>
           <div className="time">
             <img src={clock} alt="clock" /> 3 days left
@@ -29,7 +33,7 @@ function App() {
 
         <hr />
         <div className="creator">
-            <img src={avatar} alt="avatar" />
+          <img src={avatar} alt="avatar" />
           <div className="creator-desc">
             Creation of <span>Jules Wyvern</span>
           </div>
